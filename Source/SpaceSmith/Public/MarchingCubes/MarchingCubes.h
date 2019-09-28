86f210abf95fae99d99f5090c9d8267c62b4cb42
bbtarzan12 <bbtarzan12@gmail.com>
2019-09-29 00:52:16 +0900

    Marching Cubes 알고리즘 작성

---
+++ Source/SpaceSmith/Public/MarchingCubes/MarchingCubes.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include <../Plugins/Runtime/ProceduralMeshComponent/Source/ProceduralMeshComponent/Public/ProceduralMeshComponent.h>
+#include "MarchingCubes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESMITH_API UMarchingCubes : public UObject
+{
+	GENERATED_BODY()
+
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "MarchingCubes")
+	static int32 GenerateSurface
+	(
+		float IsoValue,
+		const FIntVector& ChunkStartSize,
+		const FIntVector& ChunkEndSize,
+		const FVector& ChunkLocation,
+		const FVector& ChunkScale,
+		const FTransform& TerrainTransform,
+
+		TArray<FVector>& Vertices,
+		TArray<int32>& Indices,
+		TArray<FVector>& Normals,
+		TArray<FVector2D>& UVs,
+		TArray<FLinearColor>& VertexColors,
+		TArray<FProcMeshTangent>& Tangents
+	);
+
+private:
+	static const int32 EdgeTable[256];
+	static const int32 TriTable[];
+};
